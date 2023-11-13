1b158ad89db1c0dd8d36f467052b0035952fc9ae
VishaLPatiL9029 <vishalpatil228@gmail.com>
2023-11-13 21:50:56 +0530

    Initial commit

---
+++ sensor/constant/env_variable.py
@@ -0,0 +1,4 @@
+MONGODB_URL_KEY = "mongodb://Avenger_x:vishal%40228@ac-2rrlf28-shard-00-00.33m0dww.mongodb.net:27017,ac-2rrlf28-shard-00-01.33m0dww.mongodb.net:27017,ac-2rrlf28-shard-00-02.33m0dww.mongodb.net:27017/?ssl=true&replicaSet=atlas-3v3fyi-shard-0&authSource=admin&retryWrites=true&w=majority"
+AWS_ACCESS_KEY_ID_ENV_KEY = "AWS_ACCESS_KEY_ID"
+AWS_SECRET_ACCESS_KEY_ENV_KEY = "AWS_SECRET_ACCESS_KEY"
+REGION_NAME = "us-east-1"
